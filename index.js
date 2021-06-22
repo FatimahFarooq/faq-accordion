57f4706772ce04ebc5cbd8e7179c2a10fcc8da64
FatimahFarooq <fatimafarooq47@gmai.com>
2021-06-22 11:07:57 +0300

    initial-commit-project-complete

---
+++ index.js
@@ -0,0 +1,35 @@
+// const what = () => ({
+//     alert("hello");
+// });
+
+const showAnswer = (obj) => {
+  //   alert(obj);
+  qst_container = obj;
+
+  var arrow_down = qst_container.lastElementChild;
+  if (arrow_down.classList.contains("flip-arrow")) {
+    arrow_down.classList.remove("flip-arrow");
+  } else {
+    arrow_down.classList.add("flip-arrow");
+  }
+  // arrow_down.style.transform = "rotate(180deg)";
+  question_div = qst_container.firstElementChild;
+
+  qst_div = question_div.firstElementChild;
+  if (qst_div.classList.contains("bold-qst")) {
+    qst_div.classList.remove("bold-qst");
+  } else {
+    qst_div.classList.add("bold-qst");
+  }
+
+  // qst_div.style.fontWeight = 700;
+  // qst_div.style.color = "hsl(238, 29%, 16%)";
+
+  ans_div = question_div.lastElementChild;
+  if (ans_div.classList.contains("show-answer")) {
+    ans_div.classList.remove("show-answer");
+  } else {
+    ans_div.classList.add("show-answer");
+  }
+  // ans_div.style.display = "block";
+};
